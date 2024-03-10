8b3228cb001308b235ab501ca44d76380f9e855e
Pan <62833867+eepj@users.noreply.github.com>
2024-03-10 20:09:58 +0800

    Define model and dataset

---
+++ models/cnn.py
@@ -0,0 +1,252 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms.v2 as transforms
+
+from io import StringIO
+from tqdm import tqdm
+from typing import Sequence
+
+"""
+Models adapted from:
+H. Chen, Y. Lin, and T. Zhao,
+'Chinese License Plate Recognition System Based on Convolutional Neural Network',
+Highlights in Science, Engineering and Technology, vol. 34, pp. 95-102, 2023.
+"""
+
+
+class LP_CharacterRecognitionConvBlock(nn.Module):
+
+    def __init__(
+            self,
+            in_channels: int,
+            out_channels: int,
+            device: str = "cpu"):
+
+        super(LP_CharacterRecognitionConvBlock, self).__init__()
+
+        self.device = device
+
+        self.conv = nn.Conv2d(in_channels, out_channels,
+                              kernel_size=3, stride=1, padding="same")
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+
+        self.to(device)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        return x
+
+
+class LP_CharacterRecognitionCNN(nn.Module):
+
+    def __init__(
+            self,
+            in_channels: int,
+            out_channels: int,
+            image_size: Sequence[int],
+            layer_sizes: Sequence[int],
+            device: str = "cpu",
+            transforms: transforms.Transform | None = None):
+        super(LP_CharacterRecognitionCNN, self).__init__()
+
+        self.device = device
+
+        d = len(layer_sizes)
+        h, w = image_size
+        layer_sizes.insert(0, in_channels)
+        layer_sizes.append((h // (1 << d)) * (w // (1 << d)) * layer_sizes[-1])
+
+        self.conv = nn.ModuleList()
+
+        for c in range(d):
+            self.conv.append(LP_CharacterRecognitionConvBlock(
+                layer_sizes[c], layer_sizes[c + 1], device=self.device))
+
+        self.flatten = nn.Flatten()
+        self.dropout = nn.Dropout(0.3)
+        self.fc = nn.Linear(layer_sizes[-1], out_channels)
+
+        self.optimizer = torch.optim.Adam(self.parameters())
+        self.scheduler = torch.optim.lr_scheduler.StepLR(
+            self.optimizer, 30, gamma=0.1)
+        self.loss_fn = torch.nn.CrossEntropyLoss()
+
+        self.history = {
+            "train_loss": [],
+            "train_accuracy": [],
+            "val_loss": [],
+            "val_accuracy": [],
+        }
+
+        self.to(self.device)
+
+        self.transforms = transforms
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        for conv in self.conv:
+            x = conv(x)
+
+        x = self.flatten(x)
+        x = self.dropout(x)
+        x = self.fc(x)
+
+        return x
+
+    def train_epoch(self, dataloader: DataLoader) -> tuple[float, float]:
+        self.train()
+
+        losses = 0.0
+        total_correct = 0
+        total_samples = 0
+
+        for batch in dataloader:
+            images, labels = batch
+            if self.transforms is not None:
+                images = self.transforms(images)
+            images = images.to(self.device)
+            labels = labels.to(self.device)
+
+            self.optimizer.zero_grad()
+            outputs = self(images.float())
+
+            loss = self.loss_fn(outputs, labels)
+            loss.backward()
+            self.optimizer.step()
+            losses += loss
+
+            _, predicted = torch.max(outputs.data, 1)
+            total_samples += labels.size(0)
+            total_correct += (predicted == labels).sum().item()
+            accuracy = total_correct / total_samples
+
+        return losses.cpu().item(), accuracy
+
+    def validate_epoch(self, dataloader: DataLoader) -> tuple[float, float]:
+        self.eval()
+
+        losses = 0.0
+        total_correct = 0
+        total_samples = 0
+
+        with torch.no_grad():
+            for batch in dataloader:
+                images, labels = batch
+                images = images.to(self.device)
+                labels = labels.to(self.device)
+
+                outputs = self(images.float())
+
+                loss = self.loss_fn(outputs, labels)
+                losses += loss
+
+                _, predicted = torch.max(outputs.data, 1)
+                total_samples += labels.size(0)
+                total_correct += (predicted == labels).sum().item()
+                accuracy = total_correct / total_samples
+
+        return losses.cpu().item(), accuracy
+
+    def train_loop(self,
+                   train_dataloader: DataLoader,
+                   val_dataloader: DataLoader | None = None,
+                   epoch: int = 100,
+                   initial_lr: float | None = None) -> None:
+
+        print(f"Training on device: {self.device}")
+
+        if initial_lr is not None:
+            for g in self.optimizer.param_groups:
+                g['lr'] = initial_lr
+
+        tqdm_io = StringIO()
+        tqdm_epoch = tqdm(range(epoch), file=tqdm_io,
+                          colour="GREEN", leave=True, ascii=" ░▒██")
+        # tqdm update once before entering loop
+        print(tqdm_io.getvalue(), end="\n\033[A\r")
+
+        try:
+            for e in tqdm_epoch:
+                train_acc_loss, train_accuracy = self.train_epoch(
+                    train_dataloader)
+                train_avg_loss = train_acc_loss / train_dataloader.batch_size
+                self.history['train_loss'].append(train_avg_loss)
+                self.history['train_accuracy'].append(train_accuracy)
+
+                if val_dataloader is not None:
+                    val_acc_loss, val_accuracy = self.validate_epoch(
+                        val_dataloader)
+                    val_avg_loss = val_acc_loss / val_dataloader.batch_size
+                    self.history['val_loss'].append(val_avg_loss)
+                    self.history['val_accuracy'].append(val_accuracy)
+                    desc = ('train_accuracy', 'val_accuracy')
+                else:
+                    self.history['val_loss'].append(0.0)
+                    self.history['val_accuracy'].append(0.0)
+                    desc = ('train_accuracy', 'train_loss')
+
+                self.scheduler.step()
+
+                # Print metrics
+                m0 = desc[0], self.history[desc[0]][-1]
+                m1 = desc[1], self.history[desc[1]][-1]
+                lr_epoch = self.optimizer.param_groups[0]['lr']
+
+                # Work around for updating two lines
+                print(tqdm_io.getvalue(), end="\n")
+                print(
+                    f"Epoch {e+1}: {m0[0]}: {m0[1]:5f}, {m1[0]}: {m1[1]:5f}, lr: {lr_epoch}", end="\033[A\r")
+
+            # tqdm to update once more after loop ends
+            print(tqdm_io.getvalue(), end="\n")
+            print(
+                f"Trained {e+1} epochs: {m0[0]}: {m0[1]:5f}, {m1[0]}: {m1[1]:5f}, lr: {lr_epoch}", end="\033[A\r\n\n")
+
+        except KeyboardInterrupt:
+            pass
+
+    def predict(self, dataset: Dataset) -> tuple[torch.Tensor]:
+        self.eval()
+
+        probs = torch.Tensor([]).to(self.device)
+        labels = torch.Tensor([]).to(self.device)
+        images = torch.Tensor([]).to(self.device)
+
+        print(f"Evaluating on device: {self.device}")
+
+        with torch.no_grad():
+            tqdm_io = StringIO()
+            tqdm_dataset = tqdm(dataset, file=tqdm_io,
+                                colour="GREEN", leave=True, ascii=" ░▒██")
+
+            for sample in tqdm_dataset:
+                image, label = sample
+                image = image.to(self.device)
+                label = label.to(self.device)
+
+                output = self(image.float())
+                output = torch.softmax(output, dim=1)
+
+                probs = torch.cat((probs, output))
+                labels = torch.cat(
+                    (labels, torch.Tensor([label]).to(self.device)))
+                images = torch.cat([images, image])
+
+                # For consistency with `train_epoch` function
+                print(tqdm_io.getvalue(), end="\r")
+
+            # tqdm to update once more after loop ends
+            print(tqdm_io.getvalue(), end="\r")
+
+        return probs, labels, images
+
+    def clear_history(self):
+        for key in self.history.keys():
+            self.history[key].clear()
---
+++ models/dataset.py
@@ -0,0 +1,42 @@
+import torch
+from torch.utils.data import Dataset
+import torchvision
+from torchvision.io import read_image
+import torchvision.transforms.v2 as transforms
+
+import os
+from typing import Sequence
+
+
+class LP_CharacterDataset(Dataset):
+
+    def __init__(
+        self,
+        image_dir: str,
+        image_paths: Sequence[str],
+        labels: Sequence[str],
+        label_map: dict | None = None
+    ):
+
+        self.image_dir = image_dir
+        self.image_paths = image_paths
+        self.labels = labels
+        self.label_map = label_map
+        self.preprocessor = transforms.Compose([
+            transforms.ToImage(),
+            transforms.ToDtype(torch.uint8, scale=True)
+        ])
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, index: int):
+        image_path = os.path.join(
+            self.image_dir, self.image_paths[index])
+        image = read_image(image_path)
+        label = self.labels[index]
+
+        if self.label_map:
+            label = self.label_map[label]
+
+        return image, label
